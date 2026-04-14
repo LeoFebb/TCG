ff69d27116127dec4e1140ed91ce3eabeacfeb04
LeoFebb <tua@email.com>
2026-04-14 14:51:18 +0200

    modifiche DB

--- app/Models/TcgCardCatalog.php
+++ app/Models/TcgCardCatalog.php
@@ -8,5 +8,5 @@ class TcgCardCatalog extends Model
 {
     protected $table = 'tcg_cards_catalog';
 
-    protected $fillable = ['name', 'set_name', 'card_number', 'tcg_category', 'rarity', 'image_url'];
+    protected $fillable = ['name', 'set_name', 'card_number', 'tcg_category', 'rarity', 'image_url', 'type'];
 }
